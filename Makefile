83bb8bd33b5f82710feddbf134556c8799503966
Allena Zane <azane@mi-n6.msk.21-school.ru>
2022-04-06 21:02:37 +0300

    Done

--- Makefile
+++ Makefile
@@ -1,22 +1,39 @@
 NAME = pipex
+NAME_BONUS = pipex_bonus
 CC = gcc
 CFLAGS = -Wall -Werror -Wextra
 SRC_FILES = pipex.c
 OBJ_FILES = $(SRC_FILES:.c=.o)
+SRC_FILES_BONUS = pipex_bonus.c
+OBJ_FILES_BONUS = $(SRC_FILES_BONUS:.c=.o)
 HEADER = pipex.h
 RM = rm -rf
 all:	libft $(NAME)
+
 libft:  libft/libft.a
+
 libft/libft.a:
 	$(MAKE) -C libft
+
 %.o: %.c $(HEADER)
 	$(CC) $(CFLAGS) -c $<
+
 $(NAME): $(OBJ_FILES)
 	$(CC) $(CFLAGS) $^ libft/libft.a -o $@
+
 clean:
 	$(MAKE) fclean -C libft
 	$(RM) $(OBJ_FILES)
+	$(RM) $(OBJ_FILES_BONUS)
+
 fclean: clean
-	$(RM) $(NAME)
+	$(RM) $(NAME) $(NAME_BONUS)
+
 re: fclean all
+
+$(NAME_BONUS): $(OBJ_FILES_BONUS)
+	$(CC) $(CFLAGS) $^ libft/libft.a -o $@
+
+bonus: libft $(NAME_BONUS)
+
 .PHONY: libft all clean fclean re
--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: azane <azane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 19:42:47 by azane             #+#    #+#             */
-/*   Updated: 2022/03/29 23:48:39 by azane            ###   ########.fr       */
+/*   Updated: 2022/04/06 20:51:28 by azane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,21 @@ typedef struct s_norm
 	int		here_doc;
 }	t_norm;
 
-int	ft_here_doc(t_norm *norm, char **argv)
+int	ft_here_doc(t_norm *norm, char **argv, int argc)
 {
 	int	result;
 
 	result = 0;
 	if (ft_strncmp(argv[1], "here_doc", ft_strlen("here_doc")) == 0)
 	{
-		result = 2;
+		result = 1;
 		norm->file_in = open("here_doc", O_RDWR | O_CREAT, 0777);
+		norm->file_out = open(argv[argc - 1], O_CREAT | O_WRONLY | O_APPEND,
+				0777);
 		while (1)
 		{
 			norm->line = get_next_line(0);
-			if (!ft_strncmp(norm->line, argv[2], ft_strlen(argv[2]))) 
+			if (!ft_strncmp(norm->line, argv[2], ft_strlen(argv[2])))
 			{
 				free(norm->line);
 				break ;
@@ -47,15 +49,16 @@ int	ft_here_doc(t_norm *norm, char **argv)
 			free(norm->line);
 		}
 		close(norm->file_in);
+		norm->file_in = open("here_doc", O_RDWR | O_CREAT, 0777);
 	}
 	return (result);
 }
 
 void	ft_two(t_norm *norm, int argc, char **argv, char *env[])
 {
-	if (norm->i == 2)
+	if (norm->i == 2 + norm->here_doc)
 		dup2(norm->file_in, 0);
-	if (norm->i > 2)
+	if (norm->i > norm->here_doc + 2)
 	{
 		dup2(norm->fifo[1 - norm->cur_pipe][0], 0);
 		close(norm->fifo[1 - norm->cur_pipe][0]);
@@ -66,12 +69,12 @@ void	ft_two(t_norm *norm, int argc, char **argv, char *env[])
 		close(norm->fifo[norm->cur_pipe][0]);
 		close(norm->fifo[norm->cur_pipe][1]);
 	}
-	if (norm->i + norm->here_doc == argc - 2)
+	if (norm->i == argc - 2)
 		dup2(norm->file_out, 1);
 	norm->tmp = (char **) ft_ec_malloc(3 * sizeof(char *));
 	norm->tmp[0] = ft_strdup("./pipex");
 	norm->tmp[1] = ft_strdup("-c");
-	norm->tmp[2] = ft_strdup(argv[norm->i + norm->here_doc]);
+	norm->tmp[2] = ft_strdup(argv[norm->i]);
 	if (execve("/bin/sh", norm->tmp, env) == -1)
 		ft_fatal("execve");
 	norm->k = 0;
@@ -92,9 +95,22 @@ void	ft_one(t_norm *norm, int argc, char **argv, char *env[])
 	close(norm->fifo[1 - norm->cur_pipe][0]);
 	close(norm->fifo[norm->cur_pipe][1]);
 	norm->cur_pipe = 1 - norm->cur_pipe;
-	norm->i++;
 }
 
+void	ft_zero(t_norm *norm, int argc, char **argv, char *env[])
+{
+	norm->fifo[0][0] = -1;
+	norm->fifo[0][1] = -1;
+	norm->fifo[1][0] = -1;
+	norm->fifo[1][1] = -1;
+	norm->cur_pipe = 0;
+	norm->i += norm->here_doc;
+	while (norm->i < argc - 1)
+	{
+		ft_one(norm, argc, argv, env);
+		norm->i++;
+	}
+}
 
 int	main(int argc, char **argv, char *env[])
 {
@@ -103,22 +119,20 @@ int	main(int argc, char **argv, char *env[])
 	if (argc < 5)
 		ft_fatal("wrong input");
 	norm.i = 2;
-	norm.here_doc = ft_here_doc(&norm, argv);
+	norm.here_doc = ft_here_doc(&norm, argv, argc);
 	if (!norm.here_doc)
+	{
+		norm.file_out = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC,
+				0777);
 		norm.file_in = open(argv[1], O_RDWR, 0777);
-	norm.file_out = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	}
 	if (norm.file_in == -1 || norm.file_out == -1)
 		ft_fatal("open");
-	norm.fifo[0][0] = -1;
-	norm.fifo[0][1] = -1;
-	norm.fifo[1][0] = -1;
-	norm.fifo[1][1] = -1;
-	norm.cur_pipe = 0;
-	while (norm.i + norm.here_doc < argc - 1)
-		ft_one(&norm, argc, argv, env);
+	ft_zero(&norm, argc, argv, env);
 	close(norm.fifo[1 - norm.cur_pipe][0]);
 	norm.i = -1;
-	while (++norm.i < argc - 3 - norm.here_doc)
+	while (++norm.i < argc - 3)
 		wait(0);
+	unlink("here_doc");
 	return (0);
 }
